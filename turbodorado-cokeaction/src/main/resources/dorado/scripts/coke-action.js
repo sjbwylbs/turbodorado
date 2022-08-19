c55bec5ff98ee34829b79b2aaeb59b4bb5fdb450
xiaobu <cnxobo@gmail.com>
2022-08-19 23:50:18 +0800

    move Polyfill

--- turbodorado-cokeaction/src/main/resources/dorado/scripts/coke-action.js
+++ turbodorado-cokeaction/src/main/resources/dorado/scripts/coke-action.js
@@ -1,34 +1,4 @@
 (function(window, coke){
-  // Polyfill
-  if (typeof Object.assign !== 'function') {
-    // Must be writable: true, enumerable: false, configurable: true
-    Object.defineProperty(Object, "assign", {
-      value: function assign(target, varArgs){ // .length of function is 2
-        'use strict';
-        if (target === null || target === undefined) {
-          throw new TypeError('Cannot convert undefined or null to object');
-        }
-
-        var to = Object(target);
-
-        for (var index = 1; index < arguments.length; index++) {
-          var nextSource = arguments[index];
-
-          if (nextSource !== null && nextSource !== undefined) {
-            for (var nextKey in nextSource) {
-              // Avoid bugs when hasOwnProperty is shadowed
-              if (Object.prototype.hasOwnProperty.call(nextSource, nextKey)) {
-                to[nextKey] = nextSource[nextKey];
-              }
-            }
-          }
-        }
-        return to;
-      },
-      writable: true,
-      configurable: true
-    });
-  }
 
   coke.snapshot = function(obj){
     if (obj == null || typeof(obj) != 'object') {
@@ -1660,5 +1630,37 @@
       entity.set(prop, value);
     }
   }
+
+   // Polyfill
+  if (typeof Object.assign !== 'function') {
+    // Must be writable: true, enumerable: false, configurable: true
+    Object.defineProperty(Object, "assign", {
+      value: function assign(target, varArgs){ // .length of function is 2
+        'use strict';
+        if (target === null || target === undefined) {
+          throw new TypeError('Cannot convert undefined or null to object');
+        }
+
+        var to = Object(target);
+
+        for (var index = 1; index < arguments.length; index++) {
+          var nextSource = arguments[index];
+
+          if (nextSource !== null && nextSource !== undefined) {
+            for (var nextKey in nextSource) {
+              // Avoid bugs when hasOwnProperty is shadowed
+              if (Object.prototype.hasOwnProperty.call(nextSource, nextKey)) {
+                to[nextKey] = nextSource[nextKey];
+              }
+            }
+          }
+        }
+        return to;
+      },
+      writable: true,
+      configurable: true
+    });
+  }
+
 
 })(window, window.coke || (window.coke = {}));
